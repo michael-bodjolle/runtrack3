26558061dcd0108419ecbfb412f23246c3b1d7ca
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-07 15:42:37 +0200

    job4/index.php

---
+++ jour01/job4/script.js
@@ -0,0 +1,19 @@
+var annee = 2016;
+
+function bisextile(annee) 
+{
+    if ((annee % 4 === 0 && annee % 100 > 0) || (annee % 400 === 0)) 
+    {
+
+    return(true);
+
+    } else {
+
+        return(false);
+
+    }
+
+}
+
+console.log(bisextile(annee));
+
