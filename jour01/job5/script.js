39ebe63a77ce54a08a554a60a6bee5932335eae1
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-07 16:26:54 +0200

    job5/index.php

---
+++ jour01/job5/script.js
@@ -0,0 +1,13 @@
+function afficherjourssemaines (){
+    var jourssemaine=["Lundi","Mardi","Mercredi","Jeudi","vendredi","Samedi","Dimanche"];
+
+    for (var i = 0 ; i < jourssemaine.length; i++){
+        console.log(jourssemaine[i]);
+    }
+    // for (let index = 0; index < jourssemaine.length; index++) {
+    //     console.log(jourssemaine[index]);
+        
+    // }
+
+}
+afficherjourssemaines();
