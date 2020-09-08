b27a2c122a7581db706402006a385d08e16297de
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-08 18:22:48 +0200

    script.js

--- jour02/job2/script.js
+++ jour02/job2/script.js
@@ -1,11 +1,24 @@
-var button = document.getElementById("button");
-var showhide = document.getElementById("showhide");
-button.addEventListener("click", () => {
-    if(getComputedStyle(showhide).display != "none"){
-        showhide.style.display = "none";
-    } else {
-      showhide.style.display = "block";
-    }
-  })
+// var button = document.getElementById("button");
+// var showhide = document.getElementById("showhide");
+// button.addEventListener("click", () => {
+//     if(getComputedStyle(showhide).display != "none"){
+//         showhide.style.display = "none";
+//     } else {
+//       showhide.style.display = "block";
+//     }
+//   })
 
     
+
+var button = document.getElementById("button");
+var showhide = document.getElementById("showhide");
+function showhide() {
+    if(getComputedStyle(showhide).display != "none"){
+                showhide.style.display = "none";
+            } else {
+              showhide.style.display = "block";
+            }
+  
+  
+}
+button.addEventListener("click",showhide);
