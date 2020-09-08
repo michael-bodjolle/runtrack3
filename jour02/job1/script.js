af0bf0b24807412f1bdbffc28d36a57b2b83f4f6
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-08 14:37:16 +0200

    script.js

--- jour02/job1/script.js
+++ jour02/job1/script.js
@@ -1,17 +1,3 @@
-// var button = documentget.ElementById("button");
-// var citation  = documentget.ElementById("citation");
-// function citation() {
-    
-   
-   
-    
-// }
-
-
-
-// function citation() {
-
-
 var count = 1;
 
 document.getElementById('button').onclick = function citation() {
