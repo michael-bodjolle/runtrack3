29be96054e5a752a41367e29723ec4eefc84f187
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-08 14:32:14 +0200

    jour02/job1/script.js

---
+++ jour02/job1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    
+
+
+      
+    
+</head>
+<body>
+
+    
+</body>
+<article id="citation">La vie a beaucoup plus d’imagination que nous</article>
+<button id="button">valider</button>
+
+<script src="script.js"></script>
+</html>
+
+<!-- chercher comment savoir si un bouton est clické
+récupérer contenu d'un élément html en javascript
+afficher dans la console -->
---
+++ jour02/job1/script.js
@@ -0,0 +1,24 @@
+// var button = documentget.ElementById("button");
+// var citation  = documentget.ElementById("citation");
+// function citation() {
+    
+   
+   
+    
+// }
+
+
+
+// function citation() {
+
+
+var count = 1;
+
+document.getElementById('button').onclick = function citation() {
+    var article  = document.getElementById("citation");
+    
+    console.log(article.innerHTML)
+    
+    alert("button was clicked ");
+}
+    
