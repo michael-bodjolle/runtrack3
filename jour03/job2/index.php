c36551a3113ad7775552498eadab171a14ac1a89
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-10 17:08:54 +0200

    job2/script.js

---
+++ jour03/job2/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>Document</title>
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <div id="rangees">
+    <img id="img1" src="images/arc1" alt="arc">
+        <img id="img2" src="images/arc2" alt="arc">
+        <img id="img3" src="images/arc3" alt="arc">
+        <img id="img4" src="images/arc4" alt="arc">
+        <img id="img5" src="images/arc5" alt="arc">
+        <img id="img6" src="images/arc6" alt="arc">
+    </div>
+    <button id="button">Melanger</button>
+    <div id="melangees">
+
+    </div>
+
+
+
+
+
+
+
+</body>
+
+
+
+</html>
---
+++ jour03/job2/script.js
@@ -0,0 +1,69 @@
+// $(document).ready(function(){     
+
+//   $("#button1").click(function(){
+//       $("p").hide();
+//   });
+
+// });
+// $("#button2").click(function(){
+//   $("p").show();
+// });
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+
+
+  let rangees = $('#rangees')
+
+  // let images = [
+  //   $("#img1"),
+  //   $("#img2"),
+  //   $("#img3"),
+  //   $("#img4"),
+  //   $("#img5"),
+  //   $("#img6"),
+  // ];
+
+  function randomizeArray(imgArray) {
+    for (let index = 0; index < imgArray.length; index++) {
+      rangees.
+    }
+  }
+  randomizeArray(images)
+
+
+
+  $("#button").click(function () {
+
+    $("#rangees").prependTo("#melangees");
+
+    // x = Math.floor(Math.random()*100);
+    // y = Math.floor(Math.random()*100);
+    // var x = $('#rangee').offset();
+    // images.right = y;
+    // images.left = x;
+    // $('#rangee').offset(p);      
+
+  });
+
+
+});
+
+
+// $(document).ready(function(){   
+// $("#button").click(function(){ {
+
+
+
+
+// });
+
+
+// });
