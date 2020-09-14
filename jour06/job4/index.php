28376c2eb10599aebb8752d00c86fcb1b0698c88
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-14 17:18:46 +0200

    job4/index.php

---
+++ jour06/job4/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+
+        <nav role="navigation">
+            <div id="menuToggle">
+
+                <input type="checkbox" />
+
+
+                <span></span>
+                <span></span>
+                <span></span>
+
+
+                <ul id="menu">
+                    <a href="#">Contact</a>
+                    <a href="#">Accueil</a>
+                    <a href="#">Présentation</a>
+
+
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+
+</body>
+
+</html>
