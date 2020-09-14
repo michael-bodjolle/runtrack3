0097afb50cb11be2fce6c1ba35b18ab917d355db
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-14 10:30:13 +0200

    jour06/job1/index.php

---
+++ jour06/job1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style1.css">
+    <link rel="stylesheet" type="text/css" href="style2.css">
+    <link rel="stylesheet" type="text/css" href="style3.css">
+    <link rel="stylesheet" type="text/css" href="style4.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
