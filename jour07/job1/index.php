9d5b4b48c8e673c9598f8594a08863e9ca42752b
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-15 20:20:58 +0200

    jour07/job1/JPEG-MIG-PAPILLON-Belle-dame-2.jpg

---
+++ jour07/job1/index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">LPTF</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" href="#">Accueil <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Units</a>
+                    <a class="nav-link" href="#">Jobs</a>
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Skills</a>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+
+    <main>
+        <h1 class="titre">LaPlateforme_</h1>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="JPEG-MIG-PAPILLON-Belle-dame-2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Un papillion</h5>
+                            <p class="card-text">Un papillion, c'est un peu comme une chenille, mais avec des ailes.
+                                ne pas ingerer .</p>
+                            <a href="#" class="btn btn-primary">Commander votre propre papillion</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-8">
+                    <div class="jumbotron">
+                        <h1 class="display-4">Bonjour, le monde!</h1>
+                        <p class="lead">Il existe plusieurs visions du terme.</p>
+                        <p class="lead">Le monde est la matière, l'espace et les phénomènes qui nous sont accessible par les sens, l'expérience ou la raison .</p>
+                        <p class="lead">Le sens le plus courant désigne notre planète, la Terre, avec ses habitans, et son environnement plus ou moins naturel.</p>
+                        <hr class="my-4">
+                        <p>Le sens étendu désigne l'univer dans son enssemble.</p>
+                        <button type="button" class="btn btn-danger">Rebooter le monde</button>
+                        <div class="spinner-border text-primary" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                        <div class="float-right">
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination">
+                                    <li class="page-item">
+                                        <a class="page-link" href="#" aria-label="Previous">
+                                            <span aria-hidden="true">&laquo;</span>
+                                        </a>
+                                    </li>
+                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#" aria-label="Next">
+                                            <span aria-hidden="true">&raquo;</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-2">
+                    <div class="list-group">
+                        <button type="button" class="list-group-item list-group-item-action active">
+                            Cras justo odio
+                        </button>
+                        <button type="button" class="list-group-item list-group-item-action">Limbes</button>
+                        <button type="button" class="list-group-item list-group-item-action">Luxure</button>
+                        <button type="button" class="list-group-item list-group-item-action">Gourmandise</button>
+                        <button type="button" class="list-group-item list-group-item-action">Avarice</button>
+                        <button type="button" class="list-group-item list-group-item-action">Colère</button>
+                        <button type="button" class="list-group-item list-group-item-action">hérésie</button>
+                        <button type="button" class="list-group-item list-group-item-action">Violence</button>
+                        <button type="button" class="list-group-item list-group-item-action">Ruse et tromperie</button>
+                        <button type="button" class="list-group-item list-group-item-action">Trahison</button>
+                        <button type="button" class="list-group-item list-group-item-action">Internet Explorer</button>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3">
+                </div>
+                <div class="col-6">
+                    <div class="float-right">
+                        <p>Instalation de AI 9000</p>
+                    </div><br>
+                    <div>←|</div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped bg-warning" style="width: 90%" aria-valuenow="75" aria-valuemax="100"></div>
+                    </div>
+                    <div>|→</div>
+
+                </div>
+                <div class="col-3">
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+
+                <div class="col-2">
+
+                </div>
+
+                <div class="col-3">
+                    <b>Recevez votre copie gratuite d'internet 2!</b>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">@</span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        <div class="input-group-append">
+                            <span class="input-group-text" id="basic-addon2">@example.com</span>
+                        </div>
+                    </div>
+
+                    <label for="basic-url">URL des Internets 2 et 2,1 Beta</label>
+
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">DogeCoin</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                        <div class="input-group-append">
+                            <span class="input-group-text">.00</span>
+                        </div>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">http://133t.lptf/dkwb/berlusconimtk/</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    </div>
+                </div>
+                <div class="col-3">
+                </div>
+                <div class="col-2">
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+                <div class="col-2">
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+    <div class="mb-5">
+
+    </div>
+
+
+
+</body>
+
+</html>
