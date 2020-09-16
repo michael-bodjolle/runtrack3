afdb8ff5eccbc51be849c7aae728db8544f8dcef
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-16 15:45:07 +0200

    job01/index.php

---
+++ jour08/job01/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <nav>
+
+                <a href="#">LaPlateforme_</a>
+
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Inscription</a></li>
+                <li><a href="#">Connexion</a></li>
+                <li><a href="#">Rechercher</a></li>
+                </ul>
+                </div>
+            </nav>
+    </header>
+    <main>
+        <section id="formulaire">
+            <form action="#">
+                <p>Civilité:</p>
+                <div>
+                    <input type="radio" id="huey" name="drone" value="huey" checked>
+                    <label for="huey">Homme</label>
+                </div>
+
+                <div>
+                    <input type="radio" id="dewey" name="drone" value="dewey">
+                    <label for="dewey">Femme</label>
+                </div>
+
+                <input placeholder="Nom" id="first_name" type="text" class="validate">
+                <label for="first_name"></label>
+
+
+                <input placeholder="Prenom" id="last_name" type="text" class="validate">
+                <label for="last_name"></label>
+
+
+
+                <input placeholder="Email" id="email" type="email" class="validate">
+                <label for="email"></label>
+
+
+
+                <input placeholder="Mot de passe" id="password" type="password" class="validate">
+                <label for="password"></label>
+
+
+
+                <input placeholder="Répéter le mot de passe" id="password" type="password" class="validate">
+                <label for="password"></label>
+
+
+                <div>
+                    <input type="checkbox" id="scales" name="scales" checked>
+                    <label for="scales">Informatique</label>
+                </div>
+
+                <div>
+                    <input type="checkbox" id="horns" name="horns" checked>
+                    <label for="horns">Voyages</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="scales" name="scales" checked>
+                    <label for="scales">Sport</label>
+                </div>
+
+                <div>
+                    <input type="checkbox" id="horns" name="horns" checked>
+                    <label for="horns">Lecture</label>
+                </div>
+
+                <button type="submit" name="action">Valider
+
+                </button>
+            </form>
+        </section>
+
+    </main>
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
