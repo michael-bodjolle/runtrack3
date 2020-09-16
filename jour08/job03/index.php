74f65cf5a8dcd8159b384fd869ab8136612da144
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-16 17:15:56 +0200

    job03/index.php

---
+++ jour08/job03/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">LaPlateforme_</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Inscription</a></li>
+                        <li><a href="#">Connexion</a></li>
+                        <li><a href="#">Rechercher</a></li>
+                    </ul>
+                </div>
+            </nav>
+    </header>
+
+    <main>
+    </main>
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Liens</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Accueil</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Inscription/a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Connexion</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Rechercher</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © 2020 Copyright Text
+                
+            </div>
+        </div>
+
+    </footer>
+</body>
+
+</html>
