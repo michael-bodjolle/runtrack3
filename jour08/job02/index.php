1e284b5de63b9e278276e554879100fc107dd527
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-09-16 16:33:47 +0200

    job02/index.php

---
+++ jour08/job02/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">LaPlateforme_</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Inscription</a></li>
+                        <li><a href="#">Connexion</a></li>
+                        <li><a href="#">Rechercher</a></li>
+                    </ul>
+                </div>
+            </nav>
+    </header>
+
+    <main>
